d6f2baacc3fceb4de6b2c13f0c6e7f9d558c32a8
JJ Merelo <jjmerelo@gmail.com>
2020-05-28 13:06:01 +0200

    Describing table in sql

---
+++ Chapter-12/recipedata.sql
@@ -0,0 +1,11 @@
+create table recipedata (
+  name varchar,
+  unit varchar,
+  calories float,
+  protein float,
+  main BOOLEAN,
+  side BOOLEAN,
+  vegan BOOLEAN,
+  dairy BOOLEAN,
+  dessert BOOLEAN
+);
